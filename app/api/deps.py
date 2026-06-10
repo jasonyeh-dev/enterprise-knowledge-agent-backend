a38ffb1af1f3d2e948a4ce6eee4753bdd9c781ff
jasonyeh <yehjason851016@gmail.com>
2026-06-10 16:36:44 +0800

    get user_id function def to async to get the contextvar in the main loop

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,33 +1,38 @@
-# app/api/deps.py
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 import jwt
-from app.core.security import SECRET_KEY, ALGORITHM
+from app.core.config import settings
+from app.core.context import current_user_account
 
-# 1. 宣告 OAuth2 鎖頭
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/user/auth")
+# 1. 宣告 OAuth2 鎖頭，右上角加上一個 "Authorize" 鎖頭按鈕
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/user/auth")
 
-# 2. 建立共用的 Token 解密與驗證守衛
-def get_current_user_id(token: str = Depends(oauth2_scheme)) -> int:
+#async for let the main loop function know current_user_account has changed
+async def get_current_user_id(token: str = Depends(oauth2_scheme)) -> int:
     try:
-        # 進行 JWT 解碼
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id_str: str = payload.get("sub")
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        user_id: str = payload.get("sub")
         
-        if user_id_str is None:
+        if user_id is None:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED, 
-                detail="無效的憑證"
+                detail="Certificate Invalid"
             )
-        return int(user_id_str)
+        
+        user_account: str = payload.get("account")
+
+        current_user_account.set(user_account)
+
+
+        return int(user_id)
         
     except jwt.ExpiredSignatureError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, 
-            detail="憑證已過期，請重新登入"
+            detail="Certificate expired, Please relogin"
         )
     except jwt.InvalidTokenError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, 
-            detail="憑證驗證失敗"
+            detail="Certificate failed"
         )
