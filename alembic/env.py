39cc3a4e6cc541ab1c9e06b91a98fbfac525e390
jasonyeh <yehjason851016@gmail.com>
2026-06-17 16:55:31 +0800

    test can ignore

--- alembic/env.py
+++ alembic/env.py
@@ -6,10 +6,12 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 from app.core.config import settings
+
 # 2. get Base & Model
 from app.core.database import Base
+
 # model is essential
-from app.models.models import Document, DocumentChunk, User # noqa: F401
+from app.models.models import Document, DocumentChunk, User  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -26,6 +28,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # 4. 把 target_metadata 換成你的 Base.metadata
 target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -54,10 +57,12 @@ def run_migrations_offline() -> None:
     with context.begin_transaction():
         context.run_migrations()
 
+
 def do_run_migrations(connection):
     context.configure(connection=connection, target_metadata=target_metadata)
     with context.begin_transaction():
         context.run_migrations()
+
 
 async def run_migrations_online() -> None:
     connectable = async_engine_from_config(
@@ -70,6 +75,7 @@ async def run_migrations_online() -> None:
         await connection.run_sync(do_run_migrations)
 
     await connectable.dispose()
+
 
 if context.is_offline_mode():
     run_migrations_offline()
